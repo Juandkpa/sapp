9e704842af8b552c6951c73980fb2934cdd8596c
Juan Parra <juandkpa@gmail.com>
2018-10-21 00:38:05 -0500

    provider, testing api connection and rendering basic cards

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { RestProvider, Group } from '../../providers/rest/rest';
+//import { App, MenuController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  private groups : Group[];  
+  
+  constructor(public restProvider:RestProvider) {
+    this.groups = [];
+    this.getProducts();     
   }
-
+  
+  getProducts() {
+    this.restProvider
+      .getGroups()
+      .subscribe(
+        (groups : Group[]) => {
+          this.groups = groups;
+          console.warn("groups >> ", this.groups);
+        },
+        (err) => {
+          console.error(err);
+        }
+      )
+  }
 }
---
+++ src/providers/rest/rest.ts
@@ -0,0 +1,59 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { map } from "rxjs/operators";
+
+
+export class Group {
+  id: number;
+  name: string;
+  fragment: string;
+  image: string;
+  constructor(values: Object = {}) {
+       Object.assign(this, values);
+  }
+}
+
+/*
+  Generated class for the RestProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+
+@Injectable()
+export class RestProvider {
+  baseUrl:string = "http://localhost:3000";
+
+  constructor(private http : HttpClient) {}
+  
+  //methods
+  /**
+   * Sending a get request to /products
+   */
+  public getGroups() : Observable<Group[]>  {
+    return this.http
+      .get(this.baseUrl + '/products')
+      .pipe(        
+        map (ans => Object.keys(ans).map(k=> new Group(ans[k])))
+      )
+  }
+
+  public createGroup(product: Group) {
+
+  }
+
+  public getGroupById(groupId: number) {    
+
+  }
+
+  public updateProduct(product: Group) {
+
+  }
+
+  public deleteProductById(groupId: number) {
+ 
+  }
+
+}
