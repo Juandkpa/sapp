e8e4c1264a16c1d6073621ab48db9d120eef8a5b
Juan Parra <juandkpa@gmail.com>
2018-10-31 19:24:33 -0500

    config update

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.use(function(req, res, next) {
  
 app.use(express.static('www'));
 //app.set('port', process.env.PORT || 5000);
-app.listen(PORT, function () {
+app.listen(PORT, "0.0.0.0", function () {
   console.log('Express server listening on port ' + PORT );
 });
